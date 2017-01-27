947680d8439fbd6071e9c6dd47f5222a407d51c0
Nicolas Daudin <nicolas.daudin@gmail.com>
2017-01-27 12:00:32 +0100

    Delete user

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -11,6 +11,9 @@ $(document).ready(function(){
 
     // Add User button click
     $('#btnAddUser').on('click',addUser);
+
+    // Delete User link click
+    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
 });
 
 // Functions ============
@@ -113,6 +116,38 @@ function addUser(event){
 	} else {
 		// if Errorcount is more than 0, error out
 		alert ('Please fill in all fields');
+		return false;
+	}
+
+};
+
+// Delte User
+function deleteUser(event){
+	event.preventDefault();
+
+	// Pop up a confirmation dialog
+	var confirmation = confirm('Are you sure you want to delete this user?');
+
+	// Check and make sure the user confirmed
+	if (confirmation === true){
+		// If they did, do our delete
+		$.ajax({
+			type : 'DELETE',
+			url : '/users/deleteuser/' + $(this).attr('rel')
+		}).done(function(response){
+			// Check for a successful (blank) respone
+			if (response.msg === ''){
+
+			} else {
+				alert('Error : ' + response.msg);
+			}
+
+			// Update the table
+			populateTable();
+		});
+
+	} else {
+		// If they said no to the confirm, do nothing
 		return false;
 	}
 };
--- routes/users.js
+++ routes/users.js
@@ -27,4 +27,15 @@ router.get('/userlist',function(req,res){
  	});
  });
 
+ /*
+  * DELETE to deleteuser  
+ */
+ router.delete('/deleteuser/:id', function(req,res){
+ 	var db = req.db;
+ 	var collection = db.get('userlist');
+ 	var userToDelete = req.params.id;
+ 	collection.remove({'_id' : userToDelete}, function(err){
+ 		res.send((err === null) ? {msg:''} : {msg:'error : ' + err});
+ 	});
+ });
 module.exports = router;
